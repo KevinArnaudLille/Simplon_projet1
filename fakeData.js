4266e607a4a8507acd18421bad2fa59bd31e1427
Kevin Arnaud <kevin.arnaud.addpro@gmail.com>
2022-02-02 20:53:14 +0100

    end of Night one

---
+++ fakeData.js
@@ -0,0 +1,54 @@
+const ARTICLES_TITLE="<h1>The 5 best puzzle games</h1>";
+const FAKE_ARTICLES_DATA = [
+    {
+        ranking:1,
+        title:"Baba Is You",
+        imgSrc:"./img/BabaIsYou.jpg",
+        imgAlt:"Baba is you videogame",
+        content:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum aspernatur dolorem dolorum tempora quaeratquae excepturi, nemo animi eum beatae unde recusandae ducimus voluptatibus, consequuntur error corrupti cumque commodi cum esse doloribus autem architecto laudantium porro! Quo doloribus reiciendis dolores inventore! Tempora aspernatur inventore ab veritatis mollitia delectus deserunt et!<br>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id, harum reprehenderit. Adipisci a, illum, ea error vitae ducimus suscipit voluptates vel laudantium nobis consectetur, quas iste expedita pariatur? Esse totam blanditiis doloremque, eius ducimus aliquid harum. Voluptate quis repudiandae autem velit corrupti, quas vitae tempora, quibusdam, omnis non sapiente neque obcaecati dolore laborum officia illum laboriosam similique. Doloribus deserunt ex nesciunt odio hic optio cumque animi suscipit similique velit nihil nostrum magnam odit voluptas voluptate cupiditate reiciendis, nulla amet facere molestias magni, ipsam ipsum temporibus dolorem. Modi sunt ut ex placeat doloremque repudiandae impedit facilis voluptate deleniti, sit alias recusandae!",
+        reviewsLinks: [
+            {
+                website:"Rock Paper Shotgun",
+                URL:"https://www.rockpapershotgun.com/baba-is-you-review"
+            },
+            {
+                website:"Gamekult",
+                URL:"https://www.gamekult.com/jeux/baba-is-you-3050877493/test.html"
+            }
+        ]
+    },
+    {
+        ranking:2,
+        title:"Opus Magnum",
+        imgSrc:"./img/OpusMagnum.jpg",
+        imgAlt:"Opus Magnum videogame",
+        content:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum aspernatur dolorem dolorum tempora quaeratquae excepturi, nemo animi eum beatae unde recusandae ducimus voluptatibus, consequuntur error corrupti cumque commodi cum esse doloribus autem architecto laudantium porro! Quo doloribus reiciendis dolores inventore! Tempora aspernatur inventore ab veritatis mollitia delectus deserunt et!<br>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id, harum reprehenderit. Adipisci a, illum, ea error vitae ducimus suscipit voluptates vel laudantium nobis consectetur, quas iste expedita pariatur? Esse totam blanditiis doloremque, eius ducimus aliquid harum. Voluptate quis repudiandae autem velit corrupti, quas vitae tempora, quibusdam, omnis non sapiente neque obcaecati dolore laborum officia illum laboriosam similique. Doloribus deserunt ex nesciunt odio hic optio cumque animi suscipit similique velit nihil nostrum magnam odit voluptas voluptate cupiditate reiciendis, nulla amet facere molestias magni, ipsam ipsum temporibus dolorem. Modi sunt ut ex placeat doloremque repudiandae impedit facilis voluptate deleniti, sit alias recusandae!",
+        reviewsLinks: [
+            {
+                website:"Rock Paper Shotgun",
+                URL:"https://www.rockpapershotgun.com/what-works-and-why-opus-magnum"
+            },
+            {
+                website:"Gamekult",
+                URL:"https://www.gamekult.com/jeux/opus-magnum-3050876957/test.html"
+            }
+        ]
+    },
+    {
+        ranking:3,
+        title:"Oxygen Not Included",
+        imgSrc:"./img/OxygenNotIncluded.jpg",
+        imgAlt:"Oxygen not included videogame",
+        content:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum aspernatur dolorem dolorum tempora quaeratquae excepturi, nemo animi eum beatae unde recusandae ducimus voluptatibus, consequuntur error corrupti cumque commodi cum esse doloribus autem architecto laudantium porro! Quo doloribus reiciendis dolores inventore! Tempora aspernatur inventore ab veritatis mollitia delectus deserunt et!<br>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id, harum reprehenderit. Adipisci a, illum, ea error vitae ducimus suscipit voluptates vel laudantium nobis consectetur, quas iste expedita pariatur? Esse totam blanditiis doloremque, eius ducimus aliquid harum. Voluptate quis repudiandae autem velit corrupti, quas vitae tempora, quibusdam, omnis non sapiente neque obcaecati dolore laborum officia illum laboriosam similique. Doloribus deserunt ex nesciunt odio hic optio cumque animi suscipit similique velit nihil nostrum magnam odit voluptas voluptate cupiditate reiciendis, nulla amet facere molestias magni, ipsam ipsum temporibus dolorem. Modi sunt ut ex placeat doloremque repudiandae impedit facilis voluptate deleniti, sit alias recusandae!",
+        reviewsLinks: [
+            {
+                website:"Rock Paper Shotgun",
+                URL:"https://www.rockpapershotgun.com/have-you-played-oxygen-not-included"
+            },
+            {
+                website:"Gamekult",
+                URL:"https://www.gamekult.com/jeux/oxygen-not-included-3050714249/test.html"
+            }
+        ]
+    }
+]
--- script.js
+++ script.js
@@ -2,3 +2,56 @@
 function $(selector) {
   return document.querySelector(selector);
 }
+
+// -- Generate articles content from fakedata
+const generateReview = review => {
+  return `
+  <br/>
+  <a href="${review.URL}">${review.website}</a>
+  `
+}
+const generateArticle = article => {
+  return `
+  <article>
+  <h2>${article.ranking} - ${article.title}</h2>
+  <img src="${article.imgSrc}" alt="${article.imgAlt}">
+  <p>${article.content}<p/>
+  <div class="seeReview">
+  See more on reviews:
+  ${article.reviewsLinks.map(generateReview).join('')}
+  </div>
+  </article>
+  `
+}
+const articlesSectionInnerHTML = `
+${ARTICLES_TITLE}
+${FAKE_ARTICLES_DATA.map(generateArticle).join('')}
+`;
+
+$("#bestGamesSection").innerHTML = articlesSectionInnerHTML;
+
+
+// -- Update color CSS color variables while switching theme (light/dark)
+// - update function
+let root = document.documentElement;
+let ligthBubble = $("#lightBubble img");
+let isOnDarkMode = false;
+const switchMode = () => {
+  if (isOnDarkMode) {
+    root.style.setProperty("--text-color", "black");
+    root.style.setProperty("--main-background-color", "white");
+    root.style.setProperty("--article-background-color", "#BAD4DE");
+    root.style.setProperty("--navbar-hover-color", "#BAD4DE");
+    ligthBubble.src = "./icons/lightbubble-black.svg";
+  } else {
+    root.style.setProperty("--text-color", "white");
+    root.style.setProperty("--main-background-color", "black");
+    root.style.setProperty("--article-background-color", "#000E13");
+    root.style.setProperty("--navbar-hover-color", "#082E3F");
+    ligthBubble.src = "./icons/lightbubble-white.svg";
+  }
+  isOnDarkMode = !isOnDarkMode;
+};
+// - update event
+let darkBtn = $("#lightBubble");
+darkBtn.addEventListener("click", switchMode);
