ad6ab43ca594ce8aedfe38ff52424782cfca6914
Kevin Arnaud <kevin.arnaud.addpro@gmail.com>
2022-02-02 16:02:15 +0100

    Day one - 16h

--- script.js
+++ script.js
@@ -2,10 +2,3 @@
 function $(selector) {
   return document.querySelector(selector);
 }
-
-let upGamesImg = $(".upGamesTile img");
-console.log(upGamesImg);
-upGamesImg.addEventListener("mouseover", () => console.log("over"))
-
-let numb = $(".upGamesTile").children
-console.log(numb);
